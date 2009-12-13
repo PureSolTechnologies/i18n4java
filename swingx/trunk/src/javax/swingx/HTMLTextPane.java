27b6037f51a0ad711c4c41a714d34a70346b4126
rl719236 <rl719236@3f47d3ee-cbf5-441b-837e-787c74854848>
2009-12-13 19:39:15 +0000

    - added a new element to show HTML from strings
    git-svn-id: https://i18n4java.svn.sourceforge.net/svnroot/i18n4java@58 3f47d3ee-cbf5-441b-837e-787c74854848

---
+++ swingx/trunk/src/javax/swingx/HTMLTextPane.java
@@ -0,0 +1,54 @@
+package javax.swingx;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTextPane;
+import javax.swing.event.HyperlinkEvent;
+import javax.swing.event.HyperlinkListener;
+import javax.swing.text.Document;
+import javax.swing.text.html.HTMLEditorKit;
+
+import org.apache.log4j.Logger;
+
+public class HTMLTextPane extends JTextPane implements HyperlinkListener {
+
+	private static final long serialVersionUID = 4941020106098403909L;
+
+	private static final Logger logger = Logger.getLogger(HTMLTextPane.class);
+
+	public HTMLTextPane() {
+		super();
+		init();
+	}
+
+	private void init() {
+		HTMLEditorKit kit = new HTMLEditorKit();
+		setEditorKit(kit);
+		Document doc = kit.createDefaultDocument();
+		setDocument(doc);
+		setEditable(false);
+		addHyperlinkListener(this);
+	}
+
+	@Override
+	public void hyperlinkUpdate(HyperlinkEvent event) {
+		if (event.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+			try {
+				Desktop.getDesktop().browse(event.getURL().toURI());
+			} catch (IOException e) {
+				logger.error(e.getMessage(), e);
+				JOptionPane.showConfirmDialog(Application.getInstance(),
+						"Could not open link!", "IO Error",
+						JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE);
+			} catch (URISyntaxException e) {
+				logger.error(e.getMessage(), e);
+				JOptionPane.showConfirmDialog(Application.getInstance(),
+						"Could not open link!", "IO Error",
+						JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE);
+			}
+		}
+	}
+}
