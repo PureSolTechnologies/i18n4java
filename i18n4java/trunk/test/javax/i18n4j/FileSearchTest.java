65ae3c27e9934f0d2bb6bbed33fa208ded335783
rl719236 <rl719236@3f47d3ee-cbf5-441b-837e-787c74854848>
2009-12-28 18:01:44 +0000

    - changes return parameter type for FileSearch
    git-svn-id: https://i18n4java.svn.sourceforge.net/svnroot/i18n4java@68 3f47d3ee-cbf5-441b-837e-787c74854848

--- i18n4java/trunk/test/javax/i18n4j/FileSearchTest.java
+++ i18n4java/trunk/test/javax/i18n4j/FileSearchTest.java
@@ -1,7 +1,7 @@
 package javax.i18n4j;
 
 import java.io.File;
-import java.util.Vector;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
@@ -10,23 +10,24 @@ import junit.framework.TestCase;
 
 public class FileSearchTest extends TestCase {
 
-	@Test
-	public void testFileSearch() {
-		Vector<File> files = FileSearch.find("**/*.java");
-		// check for correct file extension...
-		boolean foundSelf = false;
-		boolean foundClass = false;
-		for (File file : files) {
-			Assert.assertTrue(file.getName().endsWith(".java"));
-			if (file.getPath().endsWith(
-					"/test/javax/i18n4j/FileSearchTest.java")) {
-				foundSelf = true;
-			}
-			if (file.getPath().endsWith("/src/javax/i18n4j/FileSearch.java")) {
-				foundClass = true;
-			}
-		}
-		Assert.assertTrue(foundSelf);
-		Assert.assertTrue(foundClass);
+    @Test
+    public void testFileSearch() {
+	ArrayList<File> files = FileSearch.find("**/*.java");
+	// check for correct file extension...
+	boolean foundSelf = false;
+	boolean foundClass = false;
+	for (File file : files) {
+	    Assert.assertTrue(file.getName().endsWith(".java"));
+	    if (file.getPath().endsWith(
+		    "/test/javax/i18n4j/FileSearchTest.java")) {
+		foundSelf = true;
+	    }
+	    if (file.getPath().endsWith(
+		    "/src/javax/i18n4j/FileSearch.java")) {
+		foundClass = true;
+	    }
 	}
+	Assert.assertTrue(foundSelf);
+	Assert.assertTrue(foundClass);
+    }
 }
