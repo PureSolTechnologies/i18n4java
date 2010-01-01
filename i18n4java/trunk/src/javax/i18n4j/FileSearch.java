33d42596aa65c6bae39f94d4570655a1872b4498
rl719236 <rl719236@3f47d3ee-cbf5-441b-837e-787c74854848>
2010-01-01 17:12:57 +0000

    - minor update
    git-svn-id: https://i18n4java.svn.sourceforge.net/svnroot/i18n4java@76 3f47d3ee-cbf5-441b-837e-787c74854848

--- i18n4java/trunk/src/javax/i18n4j/FileSearch.java
+++ i18n4java/trunk/src/javax/i18n4j/FileSearch.java
@@ -25,76 +25,74 @@ import java.util.regex.Pattern;
 
 public class FileSearch {
 
-    static public ArrayList<File> find(File directory, String pattern) {
-	ArrayList<File> files = find(directory.getPath() + "/" + pattern);
-	for (int index = 0; index < files.size(); index++) {
-	    File file = files.get(index);
-	    String fileString =
-		    file.getPath().toString().substring(
-			    directory.getPath().length());
-	    files.set(index, new File(fileString));
+	static public ArrayList<File> find(File directory, String pattern) {
+		ArrayList<File> files = find(directory.getPath() + "/" + pattern);
+		for (int index = 0; index < files.size(); index++) {
+			File file = files.get(index);
+			String fileString = file.getPath().substring(
+					directory.getPath().length());
+			files.set(index, new File(fileString));
+		}
+		return files;
 	}
-	return files;
-    }
 
-    /**
-     * This method scans a directory Apache Foundation like with path
-     * pattern /some/thing/here/ * * / *.xml.
-     * 
-     * @param pattern
-     *            a Apache like directory pattern.
-     * @return A Vector of found File classes is returned.
-     */
-    static public ArrayList<File> find(String pattern) {
-	ArrayList<File> files = new ArrayList<File>();
-	File source = new File(pattern);
-	if (source.isFile()) {
-	    files.add(source);
-	} else {
-	    boolean recursive = false;
-	    if (pattern.contains("*")) {
-		pattern = source.getPath();
-		if (pattern.contains("**")) {
-		    recursive = true;
+	/**
+	 * This method scans a directory Apache Foundation like with path pattern
+	 * /some/thing/here/ * * / *.xml.
+	 * 
+	 * @param pattern
+	 *            a Apache like directory pattern.
+	 * @return A Vector of found File classes is returned.
+	 */
+	static public ArrayList<File> find(String pattern) {
+		ArrayList<File> files = new ArrayList<File>();
+		File source = new File(pattern);
+		if (source.isFile()) {
+			files.add(source);
+		} else {
+			boolean recursive = false;
+			if (pattern.contains("*")) {
+				pattern = source.getPath();
+				if (pattern.contains("**")) {
+					recursive = true;
+				}
+				String directory = pattern.replaceAll("\\*.*$", "");
+				directory = directory.replaceAll("/[^/]*$", "");
+				if (directory.isEmpty()) {
+					directory = ".";
+				}
+				source = new File(directory);
+				pattern = pattern.replaceAll("\\.", "\\\\.");
+				pattern = pattern.replaceAll("\\*", ".*");
+				pattern = pattern.replaceAll("/\\.\\*\\.\\*/", "/(.*/)*");
+			} else {
+				pattern += "/.*";
+			}
+			if (source.isDirectory()) {
+				files.addAll(findFilesInDirectory(source, pattern, recursive));
+			}
 		}
-		String directory = pattern.replaceAll("\\*.*$", "");
-		directory = directory.replaceAll("/[^/]*$", "");
-		if (directory.isEmpty()) {
-		    directory = ".";
-		}
-		source = new File(directory);
-		pattern = pattern.replaceAll("\\.", "\\\\.");
-		pattern = pattern.replaceAll("\\*", ".*");
-		pattern = pattern.replaceAll("/\\.\\*\\.\\*/", "/(.*/)*");
-	    } else {
-		pattern += "/.*";
-	    }
-	    if (source.isDirectory()) {
-		files.addAll(findFilesInDirectory(source, pattern,
-			recursive));
-	    }
+		return files;
 	}
-	return files;
-    }
 
-    static private Vector<File> findFilesInDirectory(File directory,
-	    String pattern, boolean recursive) {
-	Vector<File> files = new Vector<File>();
-	String[] fileList = directory.list();
-	for (int index = 0; index < fileList.length; index++) {
-	    File file =
-		    new File(directory.getPath() + "/" + fileList[index]);
-	    if (file.isFile()) {
-		if (Pattern.matches(pattern, file.getPath())) {
-		    files.add(file);
+	static private Vector<File> findFilesInDirectory(File directory,
+			String pattern, boolean recursive) {
+		Vector<File> files = new Vector<File>();
+		String[] fileList = directory.list();
+		for (int index = 0; index < fileList.length; index++) {
+			File file = new File(directory.getPath() + "/" + fileList[index]);
+			if (file.isFile()) {
+				if (Pattern.matches(pattern, file.getPath())) {
+					files.add(file);
+				}
+			} else if (file.isDirectory()) {
+				if (recursive) {
+					files
+							.addAll(findFilesInDirectory(file, pattern,
+									recursive));
+				}
+			}
 		}
-	    } else if (file.isDirectory()) {
-		if (recursive) {
-		    files.addAll(findFilesInDirectory(file, pattern,
-			    recursive));
-		}
-	    }
+		return files;
 	}
-	return files;
-    }
 }
