86dceb5052a1a5599e31ea1352d9d7c78ffb46dd
rl719236 <rl719236@3f47d3ee-cbf5-441b-837e-787c74854848>
2010-01-04 19:01:53 +0000

    - generalized FreeList via AbstractExtendedList - added AbstractExtendedComboBox and FreeComboBox
    git-svn-id: https://i18n4java.svn.sourceforge.net/svnroot/i18n4java@79 3f47d3ee-cbf5-441b-837e-787c74854848

---
+++ swingx/trunk/src/javax/swingx/AbstractExtendedComboBox.java
@@ -0,0 +1,54 @@
+package javax.swingx;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+
+/**
+ * This class is basis for FreeList and EnumList. It's used to provide a basic
+ * functionality to connect a output string to a free object for selection.
+ * 
+ * @author Rick-Rainer Ludwig
+ * 
+ */
+abstract public class AbstractExtendedComboBox extends ComboBox {
+
+	private static final long serialVersionUID = 34435300495300631L;
+
+	private Hashtable<Object, Object> listData = new Hashtable<Object, Object>();
+
+	public AbstractExtendedComboBox() {
+		super();
+	}
+
+	protected void setItems(ArrayList<Object> displayItems,
+			ArrayList<Object> assignedItems) {
+		removeAll();
+		if (displayItems.size() != assignedItems.size()) {
+			throw new IllegalArgumentException(
+					"The length of displayItems is unequal to assignedItems!");
+		}
+		for (int index = 0; index < displayItems.size(); index++) {
+			listData.put(displayItems.get(index), assignedItems.get(index));
+			addItem(displayItems.get(index));
+		}
+	}
+
+	protected void setItems(Hashtable<Object, Object> listData) {
+		removeAll();
+		this.listData = listData;
+		if (listData == null) {
+			return;
+		}
+		for (Object displayString : listData.keySet()) {
+			addItem((String) displayString);
+		}
+	}
+
+	public Object getSelectedItem() {
+		Object value = super.getSelectedItem();
+		if (value != null) {
+			value = listData.get(value);
+		}
+		return value;
+	}
+}
--- swingx/trunk/src/javax/swingx/FreeList.java
+++ swingx/trunk/src/javax/swingx/FreeList.java
@@ -2,72 +2,35 @@ package javax.swingx;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
-import java.util.Vector;
 
 import javax.swing.ListModel;
 
-public class FreeList extends List {
+public class FreeList extends AbstractExtendedList {
 
-    private static final long serialVersionUID =
-	    210162776409051253L;
+	private static final long serialVersionUID = 210162776409051253L;
 
-    private Hashtable<Object, Object> listData =
-	    new Hashtable<Object, Object>();
-
-    public FreeList() {
-	super();
-    }
-
-    public FreeList(ListModel listModel) {
-	super(listModel);
-    }
-
-    public void setListData(ArrayList<Object> displayItems,
-	    ArrayList<Object> assignedItems) {
-	if (displayItems.size() != assignedItems.size()) {
-	    throw new IllegalArgumentException(
-		    "The length of displayItems is unequal to assignedItems!");
+	public FreeList() {
+		super();
 	}
-	for (int index = 0; index < displayItems.size(); index++) {
-	    listData.put(displayItems.get(index),
-		    assignedItems.get(index));
-	}
-	if (listData == null) {
-	    removeAll();
-	} else if (listData.size() == 0) {
-	    removeAll();
-	} else {
-	    setListData(new Vector<Object>(displayItems));
-	}
-    }
 
-    public void setListData(
-	    Hashtable<Object, Object> listData) {
-	this.listData = listData;
-	if (listData == null) {
-	    this.removeAll();
-	} else if (listData.size() == 0) {
-	    this.removeAll();
-	} else {
-	    setListData(new Vector<Object>(listData
-		    .keySet()));
+	public FreeList(ListModel listModel) {
+		super(listModel);
 	}
-    }
 
-    public Object getSelectedValue() {
-	Object value = super.getSelectedValue();
-	if (value != null) {
-	    value = listData.get(super.getSelectedValue());
+	public void setListData(ArrayList<Object> displayItems,
+			ArrayList<Object> assignedItems) {
+		super.setListData(displayItems, assignedItems);
 	}
-	return value;
-    }
 
-    public Object[] getSelectedValues() {
-	Object[] values = super.getSelectedValues();
-	for (int index = 0; index < values.length; index++) {
-	    values[index] = listData.get(values[index]);
+	public void setListData(Hashtable<Object, Object> listData) {
+		super.setListData(listData);
 	}
-	return values;
-    }
 
+	public Object getSelectedValue() {
+		return super.getSelectedValue();
+	}
+
+	public Object[] getSelectedValues() {
+		return super.getSelectedValues();
+	}
 }
