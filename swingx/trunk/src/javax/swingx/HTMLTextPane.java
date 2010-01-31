1bfd4f8fb53998dcd8b115f284aaf93d144c95fe
rl719236 <rl719236@3f47d3ee-cbf5-441b-837e-787c74854848>
2010-01-31 19:44:59 +0000

    - some updates and enhancements
    git-svn-id: https://i18n4java.svn.sourceforge.net/svnroot/i18n4java@103 3f47d3ee-cbf5-441b-837e-787c74854848

--- swingx/trunk/src/javax/swingx/HTMLTextPane.java
+++ swingx/trunk/src/javax/swingx/HTMLTextPane.java
@@ -3,6 +3,7 @@ package javax.swingx;
 import java.awt.Desktop;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.URL;
 
 import javax.swing.JOptionPane;
 import javax.swing.JTextPane;
@@ -15,40 +16,44 @@ import org.apache.log4j.Logger;
 
 public class HTMLTextPane extends JTextPane implements HyperlinkListener {
 
-	private static final long serialVersionUID = 4941020106098403909L;
+    private static final long serialVersionUID = 4941020106098403909L;
 
-	private static final Logger logger = Logger.getLogger(HTMLTextPane.class);
+    private static final Logger logger =
+	    Logger.getLogger(HTMLTextPane.class);
 
-	public HTMLTextPane() {
-		super();
-		init();
-	}
+    public HTMLTextPane() {
+	super();
+	init();
+    }
 
-	private void init() {
-		HTMLEditorKit kit = new HTMLEditorKit();
-		setEditorKit(kit);
-		Document doc = kit.createDefaultDocument();
-		setDocument(doc);
-		setEditable(false);
-		addHyperlinkListener(this);
-	}
+    private void init() {
+	HTMLEditorKit kit = new HTMLEditorKit();
+	setEditorKit(kit);
+	Document doc = kit.createDefaultDocument();
+	setDocument(doc);
+	setEditable(false);
+	addHyperlinkListener(this);
+    }
 
-	@Override
-	public void hyperlinkUpdate(HyperlinkEvent event) {
-		if (event.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
-			try {
-				Desktop.getDesktop().browse(event.getURL().toURI());
-			} catch (IOException e) {
-				logger.error(e.getMessage(), e);
-				JOptionPane.showConfirmDialog(Application.getInstance(),
-						"Could not open link!", "IO Error",
-						JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE);
-			} catch (URISyntaxException e) {
-				logger.error(e.getMessage(), e);
-				JOptionPane.showConfirmDialog(Application.getInstance(),
-						"Could not open link!", "IO Error",
-						JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE);
-			}
+    @Override
+    public void hyperlinkUpdate(HyperlinkEvent event) {
+	if (event.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+	    try {
+		URL url = event.getURL();
+		if (url != null) {
+		    Desktop.getDesktop().browse(url.toURI());
 		}
+	    } catch (IOException e) {
+		logger.error(e.getMessage(), e);
+		JOptionPane.showConfirmDialog(Application.getInstance(),
+			"Could not open link!", "IO Error",
+			JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE);
+	    } catch (URISyntaxException e) {
+		logger.error(e.getMessage(), e);
+		JOptionPane.showConfirmDialog(Application.getInstance(),
+			"Could not open link!", "IO Error",
+			JOptionPane.OK_OPTION, JOptionPane.ERROR_MESSAGE);
+	    }
 	}
+    }
 }
