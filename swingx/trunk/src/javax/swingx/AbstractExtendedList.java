97967bf196fb733249b1ab52305e950cb103a3e2
rl719236 <rl719236@3f47d3ee-cbf5-441b-837e-787c74854848>
2010-03-28 19:19:32 +0000

    - fix in Abstract ExtendedList
    git-svn-id: https://i18n4java.svn.sourceforge.net/svnroot/i18n4java@106 3f47d3ee-cbf5-441b-837e-787c74854848

--- swingx/trunk/src/javax/swingx/AbstractExtendedList.java
+++ swingx/trunk/src/javax/swingx/AbstractExtendedList.java
@@ -7,74 +7,72 @@ import java.util.Vector;
 import javax.swing.ListModel;
 
 /**
- * This class is basis for FreeList and EnumList. It's used to provide a
- * basic functionality to connect a output string to a free object for
- * selection.
+ * This class is basis for FreeList and EnumList. It's used to provide a basic
+ * functionality to connect a output string to a free object for selection.
  * 
  * @author Rick-Rainer Ludwig
  * 
  */
 abstract public class AbstractExtendedList extends List {
 
-    private static final long serialVersionUID = 34435300495300631L;
+	private static final long serialVersionUID = 34435300495300631L;
 
-    private Hashtable<Object, Object> listData =
-	    new Hashtable<Object, Object>();
+	private Hashtable<Object, Object> listData = new Hashtable<Object, Object>();
 
-    public AbstractExtendedList() {
-	super();
-    }
-
-    public AbstractExtendedList(ListModel listModel) {
-	super(listModel);
-    }
-
-    protected void setListData(ArrayList<Object> displayItems,
-	    ArrayList<Object> assignedItems) {
-	if (displayItems.size() != assignedItems.size()) {
-	    throw new IllegalArgumentException(
-		    "The length of displayItems is unequal to assignedItems!");
+	public AbstractExtendedList() {
+		super();
 	}
-	for (int index = 0; index < displayItems.size(); index++) {
-	    listData
-		    .put(displayItems.get(index), assignedItems.get(index));
-	}
-	setListData(listData);
-    }
 
-    protected void setListData(Hashtable<Object, Object> listData) {
-	this.listData = listData;
-	if (listData == null) {
-	    this.removeAll();
-	} else if (listData.size() == 0) {
-	    this.removeAll();
-	} else {
-	    setListData(new Vector<Object>(listData.keySet()));
+	public AbstractExtendedList(ListModel listModel) {
+		super(listModel);
 	}
-    }
 
-    public void setSelectedValue(Object value, boolean scroll) {
-	for (Object key : listData.keySet()) {
-	    if (listData.get(key).equals(value) || key.equals(value)) {
-		super.setSelectedValue(key, scroll);
-	    }
+	protected void setListData(ArrayList<Object> displayItems,
+			ArrayList<Object> assignedItems) {
+		if (displayItems.size() != assignedItems.size()) {
+			throw new IllegalArgumentException(
+					"The length of displayItems is unequal to assignedItems!");
+		}
+		Hashtable<Object, Object> listData = new Hashtable<Object, Object>();
+		for (int index = 0; index < displayItems.size(); index++) {
+			listData.put(displayItems.get(index), assignedItems.get(index));
+		}
+		setListData(listData);
 	}
-    }
 
-    public Object getSelectedValue() {
-	Object value = super.getSelectedValue();
-	if (value != null) {
-	    value = listData.get(super.getSelectedValue());
+	protected void setListData(Hashtable<Object, Object> listData) {
+		this.listData = listData;
+		if (listData == null) {
+			this.removeAll();
+		} else if (listData.size() == 0) {
+			this.removeAll();
+		} else {
+			setListData(new Vector<Object>(listData.keySet()));
+		}
 	}
-	return value;
-    }
 
-    public Object[] getSelectedValues() {
-	Object[] values = super.getSelectedValues();
-	for (int index = 0; index < values.length; index++) {
-	    values[index] = listData.get(values[index]);
+	public void setSelectedValue(Object value, boolean scroll) {
+		for (Object key : listData.keySet()) {
+			if (listData.get(key).equals(value) || key.equals(value)) {
+				super.setSelectedValue(key, scroll);
+			}
+		}
 	}
-	return values;
-    }
+
+	public Object getSelectedValue() {
+		Object value = super.getSelectedValue();
+		if (value != null) {
+			value = listData.get(super.getSelectedValue());
+		}
+		return value;
+	}
+
+	public Object[] getSelectedValues() {
+		Object[] values = super.getSelectedValues();
+		for (int index = 0; index < values.length; index++) {
+			values[index] = listData.get(values[index]);
+		}
+		return values;
+	}
 
 }
