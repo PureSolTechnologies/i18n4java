1acef435943fcecdbeb3d56c56a2da59370a9a85
rl719236 <rl719236@3f47d3ee-cbf5-441b-837e-787c74854848>
2010-04-05 21:10:23 +0000

    - minor bug fix
    git-svn-id: https://i18n4java.svn.sourceforge.net/svnroot/i18n4java@108 3f47d3ee-cbf5-441b-837e-787c74854848

--- swingx/trunk/src/javax/swingx/Application.java
+++ swingx/trunk/src/javax/swingx/Application.java
@@ -12,124 +12,123 @@ import org.apache.log4j.Logger;
 
 public class Application extends Frame implements Runnable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    private static final Logger logger =
-	    Logger.getLogger(Application.class);
-    private static final Translator translator =
-	    Translator.getTranslator(Application.class);
-    private static ArrayList<Thread> threads = new ArrayList<Thread>();
+	private static final Logger logger = Logger.getLogger(Application.class);
+	private static final Translator translator = Translator
+			.getTranslator(Application.class);
+	private static ArrayList<Thread> threads = new ArrayList<Thread>();
 
-    /**
-     * This variable keeps the reference to translator.
-     */
-    static private Application instance = null;
+	/**
+	 * This variable keeps the reference to translator.
+	 */
+	static private Application instance = null;
 
-    static public Application getInstance() {
-	return instance;
-    }
-
-    static public void showNotImplementedMessage() {
-	JOptionPane.showMessageDialog(getInstance(), translator
-		.i18n("This function is not implemented yet!"), translator
-		.i18n("No Implementation"),
-		JOptionPane.INFORMATION_MESSAGE | JOptionPane.OK_OPTION);
-    }
-
-    private final String title;
-    private final String version;
-    private String subtitle;
-
-    public Application(String title, String version, String subtitle) {
-	super(title + " " + version + " [" + subtitle + "]");
-	this.title = title;
-	this.version = version;
-	this.subtitle = subtitle;
-	setInstance();
-	setClosingBehavior();
-    }
-
-    public Application(String title, String version) {
-	super(title + " " + version);
-	this.title = title;
-	this.version = version;
-	this.subtitle = "";
-	setInstance();
-	setClosingBehavior();
-    }
-
-    private synchronized void setInstance() {
-	if (instance != null) {
-	    throw new RuntimeException("Application was started double!");
+	static public Application getInstance() {
+		return instance;
 	}
-	instance = this;
-	logger.info("Application instance with name '" + getTitle()
-		+ "' was registered.");
-    }
 
-    private void setClosingBehavior() {
-	setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-	addWindowListener(new WindowAdapter() {
-	    public void windowClosing(WindowEvent e) {
-		quit();
-	    }
-	});
-    }
-
-    public String getApplicationTitle() {
-	return title;
-    }
-
-    public String getApplicationVersion() {
-	return version;
-    }
-
-    public void setApplicationSubtitle(String subTitle) {
-	this.subtitle = subTitle;
-	if (subTitle.isEmpty()) {
-	    setTitle(title + " " + version);
-	} else {
-	    setTitle(title + " " + version + " [" + subTitle + "]");
+	static public void showNotImplementedMessage() {
+		JOptionPane.showMessageDialog(getInstance(), translator
+				.i18n("This function is not implemented yet!"), translator
+				.i18n("No Implementation"), JOptionPane.INFORMATION_MESSAGE
+				| JOptionPane.OK_OPTION);
 	}
-	System.out.println(super.getTitle());
-    }
 
-    public String getSubtitle() {
-	return subtitle;
-    }
+	private final String title;
+	private final String version;
+	private String subtitle;
 
-    @Override
-    public void run() {
-	pack();
-	logger.info("Starting application...");
-	setVisible(true);
-    }
-
-    @Slot
-    public void quit() {
-	logger.info("Quitting application...");
-	stopAllThreads();
-	dispose();
-    }
-
-    private void stopAllThreads() {
-	for (Thread thread : threads) {
-	    thread.interrupt();
+	public Application(String title, String version, String subtitle) {
+		super(title + " " + version + " [" + subtitle + "]");
+		this.title = title;
+		this.version = version;
+		this.subtitle = subtitle;
+		setInstance();
+		setClosingBehavior();
 	}
-	threads.clear();
-    }
 
-    public Thread getThread(Runnable runnable) {
-	Thread thread = new Thread(runnable);
-	addThread(thread);
-	return thread;
-    }
+	public Application(String title, String version) {
+		super(title + " " + version);
+		this.title = title;
+		this.version = version;
+		this.subtitle = "";
+		setInstance();
+		setClosingBehavior();
+	}
 
-    public void addThread(Thread thread) {
-	threads.add(thread);
-    }
+	private synchronized void setInstance() {
+		if (instance != null) {
+			throw new RuntimeException("Application was started double!");
+		}
+		instance = this;
+		logger.info("Application instance with name '" + getTitle()
+				+ "' was registered.");
+	}
 
-    public void removeThreadToQuit(Thread thread) {
-	threads.remove(thread);
-    }
+	private void setClosingBehavior() {
+		setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				quit();
+			}
+		});
+	}
+
+	public String getApplicationTitle() {
+		return title;
+	}
+
+	public String getApplicationVersion() {
+		return version;
+	}
+
+	public void setApplicationSubtitle(String subTitle) {
+		this.subtitle = subTitle;
+		if (subTitle.isEmpty()) {
+			setTitle(title + " " + version);
+		} else {
+			setTitle(title + " " + version + " [" + subTitle + "]");
+		}
+		System.out.println(super.getTitle());
+	}
+
+	public String getSubtitle() {
+		return subtitle;
+	}
+
+	@Override
+	public void run() {
+		pack();
+		logger.info("Starting application...");
+		setVisible(true);
+	}
+
+	@Slot
+	public void quit() {
+		logger.info("Quitting application...");
+		stopAllThreads();
+		dispose();
+	}
+
+	private void stopAllThreads() {
+		for (Thread thread : threads) {
+			thread.interrupt();
+		}
+		threads.clear();
+	}
+
+	public Thread getThread(Runnable runnable) {
+		Thread thread = new Thread(runnable);
+		addThread(thread);
+		return thread;
+	}
+
+	public void addThread(Thread thread) {
+		threads.add(thread);
+	}
+
+	public void removeThreadToQuit(Thread thread) {
+		threads.remove(thread);
+	}
 }
