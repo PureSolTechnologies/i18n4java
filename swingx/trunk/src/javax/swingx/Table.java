ebbff2703afa23679f5c76f7cc9657788fbabdb2
rl719236 <rl719236@3f47d3ee-cbf5-441b-837e-787c74854848>
2010-04-09 17:59:11 +0000

    - added Tree - minor change in Table
    git-svn-id: https://i18n4java.svn.sourceforge.net/svnroot/i18n4java@109 3f47d3ee-cbf5-441b-837e-787c74854848

--- swingx/trunk/src/javax/swingx/Table.java
+++ swingx/trunk/src/javax/swingx/Table.java
@@ -9,74 +9,74 @@ import javax.swingx.connect.Signal;
 
 public class Table extends JTable implements ConnectionHandler {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 8468176510579836953L;
 
-	public Table() {
-		super();
-		init();
-	}
+    public Table() {
+	super();
+	init();
+    }
 
-	public Table(int rows, int cols) {
-		super(rows, cols);
-		init();
-	}
+    public Table(int rows, int cols) {
+	super(rows, cols);
+	init();
+    }
 
-	public Table(AbstractTableModel model) {
-		super(model);
-		init();
-	}
+    public Table(AbstractTableModel model) {
+	super(model);
+	init();
+    }
 
-	private void init() {
-		getSelectionModel().addListSelectionListener(this);
-	}
+    private void init() {
+	getSelectionModel().addListSelectionListener(this);
+    }
 
-	/**
-	 * This variable keeps the instance of the connection manager.
-	 */
-	protected ConnectionManager connectionManager = ConnectionManager
-			.createFor(this);
+    /**
+     * This variable keeps the instance of the connection manager.
+     */
+    protected ConnectionManager connectionManager = ConnectionManager
+	    .createFor(this);
 
-	@Override
-	public void valueChanged(ListSelectionEvent event) {
-		super.valueChanged(event);
-		selectionChanged();
-		selectionChanged(this.getSelectedRow());
-	}
+    @Override
+    public void valueChanged(ListSelectionEvent event) {
+	super.valueChanged(event);
+	selectionChanged();
+	selectionChanged(this.getSelectedRow());
+    }
 
-	@Signal
-	public void selectionChanged() {
-		connectionManager.emitSignal("selectionChanged");
-	}
+    @Signal
+    public void selectionChanged() {
+	connectionManager.emitSignal("selectionChanged");
+    }
 
-	@Signal
-	public void selectionChanged(int row) {
-		connectionManager.emitSignal("selectionChanged", row);
-	}
+    @Signal
+    public void selectionChanged(int row) {
+	connectionManager.emitSignal("selectionChanged", row);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void connect(String signal, Object receiver, String slot,
-			Class<?>... types) {
-		connectionManager.connect(signal, receiver, slot, types);
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void connect(String signal, Object receiver, String slot,
+	    Class<?>... types) {
+	connectionManager.connect(signal, receiver, slot, types);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public void release(String signal, Object receiver, String slot,
-			Class<?>... types) {
-		connectionManager.release(signal, receiver, slot, types);
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void release(String signal, Object receiver, String slot,
+	    Class<?>... types) {
+	connectionManager.release(signal, receiver, slot, types);
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public boolean isConnected(String signal, Object receiver, String slot,
-			Class<?>... types) {
-		return connectionManager.isConnected(signal, receiver, slot, types);
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isConnected(String signal, Object receiver, String slot,
+	    Class<?>... types) {
+	return connectionManager.isConnected(signal, receiver, slot, types);
+    }
 }
