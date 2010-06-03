35059d2fcafc08b0bb3228f341b9dba5c78e0ffa
rl719236 <rl719236@3f47d3ee-cbf5-441b-837e-787c74854848>
2010-06-03 19:07:15 +0000

    - minor test fix
    git-svn-id: https://i18n4java.svn.sourceforge.net/svnroot/i18n4java@116 3f47d3ee-cbf5-441b-837e-787c74854848

--- swingx/trunk/test/javax/swingx/progress/ProgressWindowTest.java
+++ swingx/trunk/test/javax/swingx/progress/ProgressWindowTest.java
@@ -1,8 +1,17 @@
 package javax.swingx.progress;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class ProgressWindowTest extends TestCase {
+
+	@Test
+	public void test() {
+		/*
+		 * This method is just for surpressing the "no test found" warning.
+		 */
+	}
 
 	public static void main(String args[]) {
 		TestProgressObservable tp = new TestProgressObservable(true);
